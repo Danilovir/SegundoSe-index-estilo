8224d8e93dde0fe35ad9bbc10359c6e0bd05783c
Danilovir <danilo.vieira7@fatec.sp.gov.br>
2024-08-12 12:30:07 -0300

    Index e estilo do Title

---
+++ SRC/Components/Title/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+import style from "./style";
+import styles from "./style";
+export default function Title(){
+    return(
+        <View style={styles.BoxTitle}>
+            <Text style={styles.texTitle}>DDM Terceiro  12/08/2024</Text>
+        </View>
+    );
+}
